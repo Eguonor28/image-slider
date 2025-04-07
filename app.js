87ee84733defacc7a3f334683855287eb923f751
Eguonor Oputu <oputueguonor@gmail.com>
2025-04-07 09:04:24 +0100

    added comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// create an array called image and store the images
 const images = [
   "image/slide2.jpg",
   "image/slide3.jpg",
@@ -6,12 +7,14 @@ const images = [
   "image/slide7.jpg",
 ];
 
+// set the initial index to 0
 let index = 0;
 const slider = document.getElementById("slider");
 
 function changeImage() {
-  index = (index + 1) % images.length;
-  slider.src = images[index];
+  index = (index + 1) % images.length; //Uses the modulus operator (%) to wrap around to 0 when reaching the end
+  slider.src = images[index]; //updates the image source of the slider element
 }
 
 const intervalId = setInterval(changeImage, 3000);
+//A built-in JavaScript function that repeatedly calls another function at fixed time intervals
